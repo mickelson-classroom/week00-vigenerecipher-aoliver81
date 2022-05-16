2005d93ec9466697ada2ec7474289f8466b29a6c
aoliver81 <98488673+aoliver81@users.noreply.github.com>
2022-05-16 13:19:00 -0600

    Add files via upload

---
+++ Vigenere Cipher.cs
@@ -0,0 +1,48 @@
+﻿//Vigenere Cipher
+//This program encrypts the charecters of a message using the Vigenere Method
+
+using System;
+using System.Diagnostics;
+class app
+{
+    //Returns true if the given character is a lowercase letter and false outherwise
+    public static bool IsLowercaseLetter(char c)
+    {
+        return c>= 'a'&& c<= 'z' ;
+    }
+    static void Tests();
+    
+    public static void TestIsLowercaseLetter()
+    {
+        Debug.Assert(IsLowercaseLetter('a'));
+        Debug.Assert(IsLowercaseLetter('b'));
+        Debug.Assert(IsLowercaseLetter('z'));
+        Debug.Assert(!IsLowercaseLetter('A'));
+        Debug.Assert(!IsLowercaseLetter('{'));
+    
+    }   
+   public static bool IsValidImput()
+    { 
+        Debug.Assert(IsValidImput('a'));
+        Debug.Assert(IsValidImput('z'));
+        Debug.Assert(!IsValidImput('A'));
+        Debug.Assert(!IsValidImput('{'));
+    {
+            
+        Debug.Assert(ShiftLetter(char);
+        Debug.Assert(ShiftLetter(char);
+        Debug.Assert(ShiftMessage)
+    }
+    static void Main()
+    {
+       TestIsLowercaseLetter();
+        Console.WriteLine("This program encrypts the characters of a message using the Vigenere method.");
+        Console.WriteLine("Please enter the message");
+        string message = Console.ReadLine();
+        Console.WriteLine("Please enter the encryption key");
+        string key = Console.ReadLine();
+        Console.WriteLine("Here is the message");
+        Console.WriteLine(message);
+        
+    }
+}
